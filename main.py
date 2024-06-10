54b00ce1e1cd607c724769c810984d82663a7bae
Danila Zagrebin <dzdanilazagrebin@gmail.com>
2024-06-10 12:50:10 +0500

    add deploy and front

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -22,29 +22,74 @@ class Employee(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/get_list', methods=['GET'])
+@app.route('/')
+def about():
+    return render_template("index.html")
+
+@app.route('/get_list')
 def get_list():
-    staff = Employee.query.all()
-    response = []
-    for employee in staff:
-        response.append({
-        'id': employee.id,
-        'name': employee.name,
-        'position': employee.position,
-        'instrument': employee.instrument
-        })
-    return jsonify(response)
+    staff = Employee.query.order_by(Employee.id.desc()).all()
+    #response = []
+    #for employee in staff:
+    #    response.append({
+    #    'id': employee.id,
+    #    'name': employee.name,
+    #    'position': employee.position,
+    #    'instrument': employee.instrument
+    #    })
+    #return jsonify(response)
+    return render_template("get_list.html", staff=staff)
 
-@app.route('/add_employee', methods=['POST'])  #добавление инф. об используемом сотрудником инструменте
+@app.route('/get_list/<int:id>')
+def get_employee(id):
+    employee = Employee.query.get(id)
+    return render_template("get_employee.html", employee=employee)
+
+@app.route('/add_employee', methods = ['POST', 'GET'])  #добавление инф. об используемом сотрудником инструменте
 def add_employee():
-    name = request.form['name']
-    position = request.form['position']
-    instrument = request.form['instrument']
-    employee = Employee(name, position, instrument)
-    db.session.add(employee)
-    db.session.commit()
-    return {"success": 'Employee added successfully'}
+    if request.method == "POST":
+        name = request.form['name']
+        instrument = request.form['instrument']
+        position = request.form['position']
 
+        employee = Employee(name = name, position = position, instrument = instrument)
+
+        try:
+            db.session.add(employee)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении сотрудника произошла ошибка"
+    else:
+        return render_template("add_employee.html")
+
+@app.route('/delete_employee/<int:id>')  #удаление инф. о сотруднике инструменте
+def del_employee(id):
+    employee = Employee.query.get(id)
+    try:
+        db.session.delete(employee)
+        db.session.commit()
+        return redirect('/get_list')
+    except:
+        return "При удалении сотрудника произошла ошибка"
+
+@app.route('/update_employee/<int:id>', methods=['POST','GET'])  #редактирование инф. о сотруднике инструменте
+def update_employee(id):
+    employee = Employee.query.get(id)
+    if request.method == "POST":
+        employee.name = request.form['name']
+        employee.position = request.form['position']
+        employee.instrument = request.form['instrument']
+
+        try:
+            db.session.commit()
+            return redirect('/get_list')
+        except:
+            return "При редактировании сотрудника произошла ошибка"
+    else:
+        employee = Employee.query.get(id)
+        return render_template("employee_update.html", employee=employee)
+"""
 @app.route('/get_employee/<int:id>')
 def get_employee(id):
     employee = Employee.query.get(id)
@@ -57,9 +102,7 @@ def get_employee(id):
         })
     else:
         return {'error': 'Employee not found'}
-
-
+"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-
